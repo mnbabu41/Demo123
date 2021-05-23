d86d40432f79535724cd79a471b00b4b1f36b0e4
mnbabu41 <53806791+mnbabu41@users.noreply.github.com>
2021-05-23 21:41:27 +0530

    second commit

---
+++ src/main/java/com/example/Springbootdemo/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.Springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	
+	@GetMapping("/hello")
+	public String Hello() {
+		
+		return "Hello";
+		
+	}
+
+}
